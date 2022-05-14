0d9069ab4d1b422572616eb33d86a287a5a07555
theoOmathimatikos <theolyber@gmail.com>
2022-05-14 20:02:15 +0300

    4.2 ready

--- usc/correct_utt2spk.py
+++ usc/correct_utt2spk.py
@@ -4,14 +4,14 @@ import os
 def corrector():
     path = os.getcwd()
     for set in ['train', 'test', 'dev']:
-        file_path = path + "/data/" + set + "/utt2spk"
+        file_path = path + "/data/" + set + "/wav.scp"
         with open(file_path, "r+") as f:
             lines = f.readlines()
 
         with open(file_path, "w+") as f:
             for l in lines:
-                f.write(l.split(" ")[0]+"_"+l.split(" ")[1][:-1] + " " +l.split(" ")[0]+"\n")
+                f.write(l.split(" ")[0] + " " + l.split(" ")[1][1:])
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     corrector()
