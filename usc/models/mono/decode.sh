e29dcb619d04e432621ce4c3a64142a0fe6478d0
theoOmathimatikos <theolyber@gmail.com>
2022-05-16 13:37:42 +0300

    push models for 4.4.3

--- usc/models/mono/decode.sh
+++ usc/models/mono/decode.sh
@@ -1,2 +1,4 @@
 steps/decode.sh --nj 4 models/mono/unigram_graph data/dev models/mono/decode_uni_dev
 steps/decode.sh --nj 4 models/mono/unigram_graph data/test models/mono/decode_uni_test
+steps/decode.sh --nj 4 models/mono/bigram_graph data/dev models/mono/decode_bi_dev
+steps/decode.sh --nj 4 models/mono/bigram_graph data/test models/mono/decode_bi_test
