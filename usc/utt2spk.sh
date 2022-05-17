8bdda7158581a0813f54999bbc35eaba2c9e90c1
theoOmathimatikos <theolyber@gmail.com>
2022-05-18 00:24:25 +0300

    push models for 4.4.3

--- usc/utt2spk.sh
+++ usc/utt2spk.sh
@@ -3,6 +3,6 @@ while read -r uttids;
 do
 temp1=$(echo $uttids | cut -d \_ -f1)
 temp2=$(echo $uttids | cut -d \_ -f2)
-echo $temp1 $temp2 >> $1
+echo $uttids $temp1 >> $1
 done < $2
 
--- usc/uttids.sh
+++ usc/uttids.sh
@@ -3,8 +3,5 @@ while read -r uttids;
 do
 temp1=$(echo $uttids | cut -d \_ -f1)
 temp2=$(echo $uttids | cut -d \_ -f2)
-echo $temp2 >> $1
-if [ $temp1 != "f1" ] ; then
-break
-fi
+echo $uttids >> $1
 done < $2
